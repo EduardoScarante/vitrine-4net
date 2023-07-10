41d0c82449c4ea452ebd150b83add2f9d8718804
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-07-10 15:15:54 -0300

    Cria componentes da aplicação

---
+++ src/modules/dash/components/detailModal.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  info: Object,
+})
+
+</script>
+
+<template>
+  <div class="container h-100 w-100 d-flex align-center justify-center">
+    <div class="content h-75 w-75 bg-white">
+      {{ info }}
+      <v-btn @click="this.$emit('close-modal')">Fechar</v-btn>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  position: fixed;
+  top: 0;
+  right: 0;
+  background-color: rgb(0, 0, 0, 0.5);
+}
+</style>
---
+++ src/modules/dash/components/itemBox.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+defineProps({
+  info: Object,
+  hover: Boolean,
+})
+
+</script>
+
+
+<template>
+    <v-card :class="hover ? 'elevation-4' : 'elevation-0'" class="ma-2" color="#F8F8F8" height="300px" width="300px">
+        {{ info }}
+        <v-btn @click="this.$emit('openDetail')">info</v-btn>
+    </v-card>
+</template>
