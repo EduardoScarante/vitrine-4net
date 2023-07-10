20efe22084e3e78ab6ac45b8d02b3dae9bef6b94
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-07-10 10:33:41 -0300

    cria template

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -1,17 +1,40 @@
 <script setup>
-
 import { useStore } from "@/composables/useStore";
+import { ref } from "vue";
+import { onMounted } from "vue";
+
 const { content } = useStore();
 const { auth } = content
 
 
-const teste = auth.user
+const email = ref('')
+const password = ref('')
 
-content.auth.teste('a', 'b')
+async function handleSignIn() {
+    const res = await auth.signIn(email.value, password.value)
+    console.log(res);
+}
+
+async function handleSignUp() {
+    const res = await auth.signUp(email.value, password.value)
+    console.log(res);
+}
+
+const user = content.auth.user
 </script>
 
-
 <template>
+    <main>
+        <input type="email" v-model="email" placeholder="email"/>
+        <input type="password" v-model="password" placeholder="senha"/>
+        <button @click="handleSignIn">Logar</button>
+    </main>
     Login
-    {{ teste }}
+    usuário -> {{ user }}
+
+    <main>
+        <input type="email" v-model="email" placeholder="email"/>
+        <input type="password" v-model="password" placeholder="senha"/>
+        <button @click="handleSignUp">Criar</button>
+    </main>
 </template>
