e7d68110009e7da2ee15fb3d7db60216cb707033
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-07-10 10:32:42 -0300

    adiciona configurações de firebase

--- firebase.config.js
+++ firebase.config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 
+
 const firebaseConfig = {
   apiKey: "AIzaSyDbRhUc2LGmgH05WnDHoyu3UCghMkLyBFA",
   authDomain: "vitrine-4net.firebaseapp.com",
@@ -9,8 +10,4 @@ const firebaseConfig = {
   appId: "1:1038176322281:web:8c142e2f2fe290124e2678"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Auth Module
-import { getAuth } from "firebase/auth";
