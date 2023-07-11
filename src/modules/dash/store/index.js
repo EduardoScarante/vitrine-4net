0166a3222fd27569d5ff1eba108cfb6bed8b904d
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-07-11 15:16:56 -0300

    Função para alimentar checkbox, itens unitários de registro

--- src/modules/dash/store/index.js
+++ src/modules/dash/store/index.js
@@ -3,9 +3,32 @@ import { getItems, createItem} from "../api/index.js";
 
 export const itemsStore = reactive({
   dbItems: [],
+  dbEvents: [],
+  dbClass: [],
+  dbYear: [],
+  
   async getItems() {
     const res = await getItems();
-    this.dbItems = res;
+    this.dbItems = res
+  
+    this.dbEvents = res.reduce((acc, curr) => {
+      if(!curr.data.evento || acc.includes(curr.data.evento)) return acc
+      acc.push(curr.data.evento)
+      return acc
+    }, [])
+
+    this.dbClass = res.reduce((acc, curr) => {
+      if(!curr.data.tipo || acc.includes(curr.data.tipo)) return acc
+      acc.push(curr.data.tipo)
+      return acc
+    }, [])
+
+    this.dbYear = res.reduce((acc, curr) => {
+      const year = curr.data.dataCompra.split("-")[0]
+      if(!year || acc.includes(year)) return acc
+      acc.push(year)
+      return acc
+    }, [])
   },
   async createItem(payload){
     const res = await createItem(payload)
