11c1e6c2f93a0cfe800ccc9fc60333cf5a9f29a7
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-07-12 13:54:12 -0300

    Atualiza componente de detalhe e itembox

--- src/modules/dash/components/detailModal.vue
+++ src/modules/dash/components/detailModal.vue
@@ -1,18 +1,43 @@
 <script setup>
 import { ref } from "vue";
 
+const newComp = ref("");
+
 defineProps({
   info: Object,
 });
+
+const editInfos = ref(false);
+
 </script>
 
 <template>
   <div class="container h-100 w-100 d-flex align-center justify-center">
     <div class="content h-75 w-75 bg-white pa-2">
-      <div class="d-flex justify-space-between">
+      <div
+        class="d-flex align-center justify-space-between"
+        :class="editInfos ? 'bg-orange' : ''"
+      >
         <div>
-          <v-btn icon="mdi-pencil" class="mr-2"></v-btn>
-          <v-btn icon="mdi-trash-can-outline"></v-btn>
+          <v-btn
+            v-if="!editInfos"
+            class="mx-2"
+            icon="mdi-pencil"
+            @click="editInfos = true"
+          ></v-btn>
+          <v-btn
+            v-if="!editInfos"
+            class="mx-2"
+            icon="mdi-trash-can-outline"
+            @click="this.$emit('delete-item', info.id)"
+          ></v-btn>
+
+          <v-btn
+            v-if="editInfos"
+            @click="this.$emit('update-item', info); editInfos = false"
+            class="mx-2"
+            icon="mdi-content-save-alert"
+          ></v-btn>
         </div>
         <h1>{{ info.data.nome }}</h1>
         <div>
@@ -23,75 +48,111 @@ defineProps({
         </div>
       </div>
 
-      <div class="d-flex">
-        <div class="w-50 d-flex">
-          <v-img
-            class="custom-border"
-            :width="300"
-            :height="450"
-            src="https://is3-ssl.mzstatic.com/image/thumb/Purple116/v4/7c/ea/f0/7ceaf0da-d4d3-3426-a95b-c592a2b3f58e/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/512x512bb.jpg"
-          ></v-img>
-        </div>
+      <v-divider class="ma-4"></v-divider>
 
-        <div class="w-50 ">
-          <div class=" ">
-            
-            <div class="">
-              <div class="">
-                <p>Finalidade:</p>
-                <p>{{ info.data.finalidade }}</p>
-                <p>Evento:</p>
-                <p>{{ info.data.evento }}</p>
-              </div>
-              <p>Categoria:</p>
-              <p>{{ info.data.tipo }}</p>
-            </div>
+      <div>
+        <v-row>
+          <v-col cols="6" class="d-flex justify-center">
+            <v-img max-height="500px" :src="info.url[0]"></v-img>
+          </v-col>
+          <v-col style="height: 580px" class="overflow-auto" cols="6">
+            <h2>Informações Técnicas</h2>
+            <v-text-field
+              v-model="info.data.altura"
+              label="Altura"
+              :readonly="!editInfos"
+              variant="underlined"
+            ></v-text-field>
+            <v-text-field
+              v-model="info.data.comp"
+              label="Comprimento"
+              :readonly="!editInfos"
+              variant="underlined"
+            ></v-text-field>
+            <v-text-field
+              v-model="info.data.larg"
+              label="Largura"
+              :readonly="!editInfos"
+              variant="underlined"
+            ></v-text-field>
+            <v-text-field
+              v-model="info.data.material"
+              label="Material"
+              :readonly="!editInfos"
+              variant="underlined"
+            ></v-text-field>
 
-            <div>
-              <p>Preço: R$</p>
-              <p>{{ info.data.preco }}</p>
-              <p>Fornecedor:</p>
-              <p>{{ info.data.fornecedor }}</p>
+            <h2>Informações Comerciais</h2>
 
-              <h3>Ano:</h3>
-              <p>{{ info.data.dataCompra }}</p>
-            </div>
-          </div>
+            <v-text-field
+              v-model="info.data.fornecedor"
+              label="Fornecedor"
+              :readonly="!editInfos"
+              variant="underlined"
+            ></v-text-field>
+            <v-text-field
+              v-model="info.data.preco"
+              label="Valor"
+              :readonly="!editInfos"
+              variant="underlined"
+            ></v-text-field>
 
-          <h2>Informações Técnicas</h2>
-          <div>
-            <h3>Altura:</h3>
-            <p>{{ info.data.altura }}</p>
-            <h3>Comprimento:</h3>
-            <p>{{ info.data.comp }}</p>
-            <h3>Largura:</h3>
-            <p>{{ info.data.larg }}</p>
-          </div>
+            <h2>Informações Gerais</h2>
+            <v-text-field
+              v-model="info.data.finalidade"
+              label="finalidade"
+              :readonly="!editInfos"
+              variant="underlined"
+            ></v-text-field>
+            <v-text-field
+              v-model="info.data.tipo"
+              label="tipo"
+              :readonly="!editInfos"
+              variant="underlined"
+            ></v-text-field>
+            <v-text-field
+              v-model="info.data.evento"
+              label="evento"
+              :readonly="!editInfos"
+              variant="underlined"
+            ></v-text-field>
+            <v-text-field
+              v-model="info.data.dataCompra"
+              label="dataCompra"
+              :readonly="!editInfos"
+              variant="underlined"
+            ></v-text-field>
 
-          <h3>Materiais:</h3>
-          <p>{{ info.data.material }}</p>
-
-          <h2>Descrição</h2>
-          <p class="custom-scroll">{{ info.data.descrição }}</p>
-        </div>
+            <h2>Descrição</h2>
+            <v-textarea
+              auto-grow
+              v-model="info.data.descrição"
+              label="Descrição"
+              :readonly="!editInfos"
+              variant="underlined"
+            ></v-textarea>
+          </v-col>
+        </v-row>
       </div>
+
+      <v-alert
+        type="warning"
+        title="Cuidado..."
+        text="Modo de edição ativo!"
+        closable=""
+        v-if="editInfos"
+      >
+      </v-alert>
     </div>
   </div>
 </template>
 
 <style scoped>
-.desc {
-  height: 650px;
-}
 .container {
   position: fixed;
   top: 0;
   right: 0;
   background-color: rgb(0, 0, 0, 0.5);
-}
-
-.custom-border {
-  border: 0.5px solid darkgray;
 }
 
 .custom-scroll {
