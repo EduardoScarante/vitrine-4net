7ced59cc449d600604cdfbc4d7155786ef573fd3
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-07-14 09:17:35 -0300

    Cria componente para criar itens

--- src/modules/dash/components/createItem.vue
+++ src/modules/dash/components/createItem.vue
@@ -21,7 +21,7 @@ function handleDefineImg(event) {
   imgRef.value = event.target.files[0];
 }
 
-const emit = defineEmits(['customChange'])
+const emit = defineEmits(["customChange"]);
 
 async function handleCreateItem() {
   const payload = {
@@ -39,68 +39,48 @@ async function handleCreateItem() {
   };
 
   const imgpayload = imgRef.value;
-  
-  emit('create-item', payload, imgpayload)
+
+  emit("create-item", payload, imgpayload);
 }
 </script>
 
 <template>
   <div class="container h-100 w-100 d-flex align-center justify-center">
-    <div class="content h-75 w-75 bg-white pa-2">
-      <v-card class="w-50 mx-auto elevation-0">
-        <div class="d-flex justify-center">
-          <h2>Criar novo Registro</h2>
-        </div>
-        <v-form>
-          <v-text-field type="file" :onchange="handleDefineImg"></v-text-field>
+    <v-card class="w-50 pa-5 mx-auto elevation-0">
+      <div class="d-flex justify-center">
+        <h2>Criar novo Registro</h2>
+        <v-btn @click="this.$emit('close-modal')">x</v-btn>
+      </div>
+      <v-form>
+        <v-text-field type="file" :onchange="handleDefineImg"></v-text-field>
 
-          <v-row>
-            <v-col>
-              <h3>Dados do Item</h3>
-              <v-text-field v-model="nome" label="Item"></v-text-field>
-              <v-text-field v-model="tipo" label="Tipo"></v-text-field>
-              <v-text-field
-                v-model="fornecedor"
-                label="Fornecedor"
-              ></v-text-field>
-              <v-text-field
-                v-model="dataCompra"
-                type="date"
-                label="Data Compra"
-              ></v-text-field>
-              <v-text-field
-                v-model="preco"
-                type="number"
-                label="Preço"
-              ></v-text-field>
+        <v-row>
+          <v-col>
+            <h3>Dados do Item</h3>
+            <v-text-field v-model="nome" label="Item"></v-text-field>
+            <v-text-field v-model="tipo" label="Tipo"></v-text-field>
+            <v-text-field v-model="fornecedor" label="Fornecedor"></v-text-field>
+            <v-text-field v-model="dataCompra" type="date" label="Data Compra"></v-text-field>
+            <v-text-field v-model="preco" type="number" label="Preço"></v-text-field>
 
-              <h3>Evento</h3>
-              <v-text-field v-model="evento" label="Evento"></v-text-field>
-              <v-text-field
-                v-model="finalidade"
-                label="Finalidade"
-              ></v-text-field>
-            </v-col>
+            <h3>Evento</h3>
+            <v-text-field v-model="evento" label="Evento"></v-text-field>
+            <v-text-field v-model="finalidade" label="Finalidade"></v-text-field>
+          </v-col>
 
-            <v-col>
-              <h3>Informações Gerais</h3>
+          <v-col>
+            <h3>Informações Gerais</h3>
 
-              <v-text-field
-                v-model="descrição"
-                label="Descrição"
-              ></v-text-field>
-              <v-text-field v-model="comp" label="Comprimento"></v-text-field>
-              <v-text-field v-model="altura" label="Altura"></v-text-field>
-              <v-text-field v-model="larg" label="Largura"></v-text-field>
-              <v-text-field v-model="material" label="Material"></v-text-field>
-            </v-col>
-          </v-row>
-          <v-btn @click="handleCreateItem" class="w-100" variant="tonal"
-            >Cadastrar</v-btn
-          >
-        </v-form>
-      </v-card>
-    </div>
+            <v-text-field v-model="descrição" label="Descrição"></v-text-field>
+            <v-text-field v-model="comp" label="Comprimento"></v-text-field>
+            <v-text-field v-model="altura" label="Altura"></v-text-field>
+            <v-text-field v-model="larg" label="Largura"></v-text-field>
+            <v-text-field v-model="material" label="Material"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-btn @click="handleCreateItem" class="w-100" variant="tonal">Cadastrar</v-btn>
+      </v-form>
+    </v-card>
   </div>
 </template>
 
