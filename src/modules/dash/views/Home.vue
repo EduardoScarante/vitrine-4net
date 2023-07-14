1e4fed33379b11006cd028d2a59a683e90601439
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-07-14 14:57:29 -0300

    Remove código sem utilidade

--- src/modules/dash/views/Home.vue
+++ src/modules/dash/views/Home.vue
@@ -105,17 +105,6 @@ const filteredItens = computed(() => {
 </script>
 
 <template>
-  <v-btn
-    @click="
-      content.items.loading == false
-        ? (content.items.loading = true)
-        : (content.items.loading = false)
-    "
-  >
-    loading
-  </v-btn>
-
-  <v-btn :loading="content.items.loading"> </v-btn>
   <!-- HEADER -->
   <div class="blueBg d-flex flex-column">
     <v-img :src="logo4net"></v-img>
