5b444517176353b1b892a772cc6225a3e00de82f
EduardoScarante <eduardo.demczuk@it4cio.com>
2023-07-17 11:41:34 -0300

    Torna variaveis de ambiente

--- firebase.config.js
+++ firebase.config.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbRhUc2LGmgH05WnDHoyu3UCghMkLyBFA",
-  authDomain: "vitrine-4net.firebaseapp.com",
-  projectId: "vitrine-4net",
-  storageBucket: "vitrine-4net.appspot.com",
-  messagingSenderId: "1038176322281",
-  appId: "1:1038176322281:web:8c142e2f2fe290124e2678"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
